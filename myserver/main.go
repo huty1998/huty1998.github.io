d480d24486d1197003dfa394cee31fbfc3c0a124
hutianyu <hutianyu@eswin.com>
2022-05-07 14:28:39 +0800

    2022-05-07

--- myserver/main.go
+++ myserver/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,8 +11,13 @@ func main() {
 
 	r := gin.Default()
 
+	//middleware
+	r.Use(m1())
+
 	r.GET("/", func(c *gin.Context) {
-		c.String(http.StatusOK, "Wish you have a great day!")
+		req, _ := c.Get("key1") //same ctx as which in middleware??
+		fmt.Println("main")
+		c.JSON(http.StatusOK, map[string]interface{}{"req": req})
 	})
 
 	r.Run(":728")
