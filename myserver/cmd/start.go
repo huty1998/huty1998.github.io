b8ece929e57cf94c1f5039fd4615024030937dbb
hutianyu <hutianyu@eswin.com>
2022-05-18 09:47:03 +0800

    2022-05-18

--- myserver/cmd/start.go
+++ myserver/cmd/start.go
@@ -2,40 +2,11 @@ package cmd
 
 import (
 	"context"
-	"fmt"
-	"myserver/middleware"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
 )
 
 func Start(ctx context.Context) {
 
 	// defer wg.Done()
-	r := gin.Default()
-
-	//middleware
-	r.Use(middleware.M1())
-
-	r.GET("/test", func(ctx *gin.Context) {
-		req, _ := ctx.Get("key1") //same ctx as which in middleware??
-		fmt.Println("main")
-		ctx.JSON(http.StatusOK, map[string]interface{}{"req": req})
-	})
-
-	r.NoRoute(func(ctx *gin.Context) { //404
-		ctx.String(http.StatusNotFound, "404 NOT FOUND, NO ROUTE")
-	})
-
-	// r.Run(":728") //gin
-
-	server := &http.Server{Addr: ":728", Handler: r} //combine this router with http.Server
-	server.ListenAndServe()                          // listen and server http request
-	<-ctx.Done()                                     //release port
-	if err := server.Shutdown(ctx); err != nil {
-		fmt.Printf("Server forced to shutdown: " + err.Error())
-	}
-
 	/*
 			syncq, _ := net.Listen("tcp", "127.0.0.1:728")
 		LOOP:
--- myserver/main.go
+++ myserver/main.go
@@ -2,16 +2,42 @@ package main
 
 import (
 	"context"
-	"myserver/cmd"
+	"fmt"
+	"myserver/middleware"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 
+	r := gin.Default()
+
+	//middleware
+	r.Use(middleware.M1())
+
+	r.GET("/test", func(ctx *gin.Context) {
+		req, _ := ctx.Get("key1") //same ctx as which in middleware??
+		fmt.Println("main")
+		ctx.JSON(http.StatusOK, map[string]interface{}{"req": req})
+	})
+
+	r.NoRoute(func(ctx *gin.Context) { //404
+		ctx.String(http.StatusNotFound, "404 NOT FOUND, NO ROUTE")
+	})
+
+	// r.Run(":728") //gin
 	ctx, cancel := context.WithCancel(context.Background())
-	cmd.Start(ctx)
+
+	server := &http.Server{Addr: ":728", Handler: r} //combine this router with http.Server
+	server.ListenAndServe()                          // listen and server http request
+	<-ctx.Done()                                     //release port
+	if err := server.Shutdown(ctx); err != nil {
+		fmt.Printf("Server forced to shutdown: " + err.Error())
+	}
 
 	c := make(chan os.Signal)
 	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT, syscall.SIGKILL, os.Interrupt)
@@ -21,5 +47,5 @@ func main() {
 	<-ctx.Done()
 	// var wg sync.WaitGroup
 	// wg.Add(1)
-	// wg.Wait()
+	// wg.Wait() https://blog.csdn.net/textdemo123/article/details/103416781
 }
