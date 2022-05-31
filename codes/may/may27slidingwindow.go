82583e2d4c920cb5224f8b61b31474db2de9bc06
hutianyu <hutianyu@eswin.com>
2022-05-31 10:07:08 +0800

    2022-05-31

--- codes/may/may27slidingwindow.go
+++ codes/may/may27slidingwindow.go
@@ -1,30 +1,33 @@
 package main
 
+import "fmt"
+
 func main() {
-	lengthOfLongestSubstring("abcddbfgsxc")
+	fmt.Println(lengthOfLongestSubstring("abcbanpf"))
 }
 
 func lengthOfLongestSubstring(s string) int {
-	if len(s) == 0 {
-		return 0
-	}
+	// if len(s) == 0 {
+	// 	return 0
+	// }
 	win := make(map[byte]int)
-	left := 0
-	right := 0
+	left, right := 0, 0
 	ans := 1
 	for right < len(s) {
-		c := s[right]
+		sr := s[right]
 		right++
-		win[c]++
-		for win[c] > 1 {
-			d := s[left]
+		win[sr]++
+		//narrow down
+		for win[sr] > 1 { //dont know where it is, only know there's a repeat
+			sl := s[left]
 			left++
-			win[d]--
+			win[sl]--
 		}
 		ans = max(right-left, ans)
 	}
 	return ans
 }
+
 func max(a, b int) int {
 	if a > b {
 		return a
