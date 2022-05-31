681338c49307a6448d6c6d2080d4350f98b23b7e
hutianyu <hutianyu@eswin.com>
2022-05-31 14:33:44 +0800

    2022-05-31

--- myserver/database/database.go
+++ myserver/database/database.go
@@ -1,11 +1,19 @@
 package database
 
 import (
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
+type Person struct {
+	Id   int
+	Name string
+}
+
 func Dbinit() {
-	Db, err = gorm.Open(sqlite.Open("./mydb.db"), &gorm.Config{})
+	Db, _ := gorm.Open(mysql.Open("./mydb.db"), &gorm.Config{})
+	Db.AutoMigrate(&Person{})
+
+	Db.Create(&Person{Id: 320323, Name: "Terry"})
 	//https://gorm.io/docs/
 }
--- myserver/go.mod
+++ myserver/go.mod
@@ -4,5 +4,6 @@ go 1.17
 
 require (
 	github.com/gin-gonic/gin v1.7.7
+	gorm.io/driver/mysql v1.3.4 // indirect
 	gorm.io/gorm v1.23.5 // indirect
 )
