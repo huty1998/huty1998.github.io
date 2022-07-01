2700fbe781ee2c6bc7d0dbc522734e3d7f394bdd
hutianyu <hutianyu@eswincomputing.com>
2022-07-01 17:25:07 +0800

    2022-07-01

--- myserver/main.go
+++ myserver/main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"myserver/database"
+	"os/exec"
+	"time"
 )
 
 func main() {
 	database.Dbinit()
+	ticker := time.NewTicker(time.Second)
+	done := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				{
+					fmt.Println("tik tik...")
+				}
+			case <-done:
+				{
+					return
+				}
+			}
+		}
+	}()
+	time.Sleep(1 * time.Second)
+	ticker.Stop()
+	done <- true
+	fmt.Println("tick finished")
+
+	cmd := exec.Command("bash", "-c", "ls -rt")
+	// cmd := exec.Command("echo", "Hi")
+	out, _ := cmd.Output()
+	fmt.Println(string(out))
 
 }
