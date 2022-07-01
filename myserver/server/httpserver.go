643b5a84f7092a777a1db31eb0a7fa173f0ed5cc
hutianyu <hutianyu@eswincomputing.com>
2022-07-01 15:48:31 +0800

    2022-07-01

--- myserver/server/httpserver.go
+++ myserver/server/httpserver.go
@@ -1,5 +1,17 @@
 package server
 
-func HttpServer() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func HttpServer() {
+	http.HandleFunc("/http", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Header, r.Body)
+	})
+
+	if err := http.ListenAndServe(":728", nil); err != nil {
+		log.Fatal("ListenAndServe err: ", err)
+	}
 }
