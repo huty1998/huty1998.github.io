dd890bd1227badbb432b98d94db5149eb81eccc5
hutianyu <hutianyu@eswincomputing.com>
2022-08-02 09:15:34 +0800

    2022-08-02

--- myserver/server/tcpserver.go
+++ myserver/server/tcpserver.go
@@ -1,5 +1,39 @@
 package server
 
-func TcpServer() {
+import (
+	"bufio"
+	"fmt"
+	"net"
+)
 
+func TcpServer() {
+	listen, err := net.Listen("tcp", "127.0.0.1:728")
+	if err != nil {
+		fmt.Println("listen failed, err:", err)
+		return
+	}
+	for {
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("accept failed, err:", err)
+			continue
+		}
+		go process(conn)
+	}
+}
+
+func process(conn net.Conn) {
+	defer conn.Close()
+	for {
+		reader := bufio.NewReader(conn)
+		var buf [128]byte
+		n, err := reader.Read(buf[:])
+		if err != nil {
+			fmt.Println("read from client failed, err:", err)
+			break
+		}
+		recvStr := string(buf[:n])
+		fmt.Println("client: ", recvStr)
+		conn.Write([]byte(recvStr))
+	}
 }
