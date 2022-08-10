9d6788546eb749cfab0180834e41735a18034c6d
hutianyu <hutianyu@eswincomputing.com>
2022-08-10 09:17:49 +0800

    2022-08-10

---
+++ _codes/aug/10_decorator_test.go
@@ -0,0 +1,29 @@
+package aug
+
+import (
+	"fmt"
+	"testing"
+)
+
+type Phone interface {
+	Slogan() string
+}
+
+type Iphone11 struct{}
+
+func (i11 Iphone11) Slogan() string {
+	return "Iphone 11"
+}
+
+type Iphone11Pro struct {
+	i11 Phone //**
+}
+
+func (i11pro Iphone11Pro) Slogan() string {
+	return i11pro.i11.Slogan() + " Pro"
+}
+
+func TestDecorator(t *testing.T) {
+	decorator := Iphone11Pro{Iphone11{}}
+	fmt.Println(decorator.Slogan())
+}
