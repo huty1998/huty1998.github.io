199c9223a7a277dc94446d63dac6331764e524c1
hutianyu <hutianyu@eswincomputing.com>
2022-08-19 16:44:50 +0800

    2022-08-19

---
+++ _codes/aug/18_bridge_test.go
@@ -0,0 +1,57 @@
+package aug
+
+import (
+	. "fmt"
+	"reflect"
+	"testing"
+)
+
+type Computer interface {
+	SetNprint()
+}
+
+type Mac struct {
+	printer Printer //Bridge: interface, not like HP/Epison
+}
+
+func (m *Mac) SetNprint(p Printer) {
+	m.printer = p
+	Printf("Build a bridge between %v and %v...\n", reflect.TypeOf(m).Elem().Name(), reflect.TypeOf(p).Elem().Name())
+	p.Print()
+}
+
+type Win struct {
+	printer Printer
+}
+
+func (m *Win) SetNprint(p Printer) {
+	m.printer = p
+	p.Print()
+}
+
+//////////////////////////////////////////////////
+type Printer interface {
+	Print()
+}
+
+type HP struct{}
+
+func (hp *HP) Print() {
+	Println("Printing by HP...")
+}
+
+type Epison struct{}
+
+func (epison *Epison) Print() {
+	Println("Printing by Epison...")
+}
+
+func TestBridge(t *testing.T) {
+	mac := &Mac{}
+	hp := &HP{}
+	epison := &Epison{}
+	mac.SetNprint(hp)
+	mac.SetNprint(epison)
+}
+
+//http://www.17bigdata.com/study/programming/it-go/it-go-234126.html
