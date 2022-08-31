2457abf6890c54d48b19d53cc0c4555671362972
hutianyu <hutianyu@eswincomputing.com>
2022-08-31 10:51:32 +0800

    2022-08-31

--- _codes/aug/31_quicksort_test.go
+++ _codes/aug/31_quicksort_test.go
@@ -17,17 +17,16 @@ func QuickSort(nums []int, start, end int) []int {
 //***//
 func partition(nums []int, start, end int) int {
 	p := start
-	for j := start; j < end; j++ {
-		if nums[j] < nums[end] {
-			nums[p], nums[j] = nums[j], nums[p]
+	for cur := start; cur < end; cur++ {
+		if nums[cur] < nums[end] {
+			nums[p], nums[cur] = nums[cur], nums[p]
 			p++
 		}
 	}
 	nums[p], nums[end] = nums[end], nums[p]
-	fmt.Println(nums)
 	return p
 }
 
 func TestQuickSort(t *testing.T) {
-	fmt.Println(QuickSort([]int{3, 7, 2, 4, 9, 10, 6, 8, 5}, 0, 8))
+	fmt.Println(QuickSort([]int{3, 7, 2, 4, 9, 10, 6}, 0, 6))
 }
