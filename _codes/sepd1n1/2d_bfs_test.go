2e28e8f8ba2574666dd8a498d6f01a4b3a7c36ed
hutianyu <hutianyu@eswincomputing.com>
2022-09-02 17:43:39 +0800

    2022-09-02

--- _codes/sepd1n1/2d_bfs_test.go
+++ _codes/sepd1n1/2d_bfs_test.go
@@ -37,3 +37,29 @@ func levelOrder(root *TreeNode) []int {
 func TestBfs(t *testing.T) {
 	fmt.Println(levelOrder(&TreeNode{1, &TreeNode{2, nil, nil}, &TreeNode{3, &TreeNode{4, nil, nil}, nil}}))
 }
+
+func levelOrder_2(root *TreeNode) [][]int {
+	result := make([][]int, 0)
+	if root == nil {
+		return result
+	}
+	queue := make([]*TreeNode, 0)
+	queue = append(queue, root)
+	for len(queue) > 0 {
+		list := make([]int, 0)
+		l := len(queue)
+		for i := 0; i < l; i++ {
+			level := queue[0]
+			queue = queue[1:]
+			list = append(list, level.Val)
+			if level.Left != nil {
+				queue = append(queue, level.Left)
+			}
+			if level.Right != nil {
+				queue = append(queue, level.Right)
+			}
+		}
+		result = append(result, list)
+	}
+	return result
+}
