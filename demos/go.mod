8c4e6982487499668283e18074a6e516d54791cb
hutianyu <hutianyu@eswincomputing.com>
2022-09-27 09:03:44 +0800

    2022-09-27

--- demos/go.mod
+++ demos/go.mod
@@ -1,3 +1,8 @@
 module demos
 
 go 1.16
+
+require (
+	github.com/containerd/cgroups v1.0.4
+	github.com/opencontainers/runtime-spec v1.0.2
+)
--- demos/main.go
+++ demos/main.go
@@ -1,25 +1,70 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"time"
+	"log"
+	"os/exec"
+	"strings"
+
+	"github.com/containerd/cgroups"
+	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
 func main() {
-	fmt.Println("Open file...")
-	filepath := "/home/hutianyu/0823.txt"
+	// fmt.Println("Open file...")
+	// filepath := "/home/hutianyu/0823.txt"
 
-	for count, max := 0, 20; count < max; count++ {
-		fd, err := os.OpenFile(filepath, os.O_RDWR, 0)
-		if err != nil {
-			fmt.Printf("fail to open %s", filepath)
-		}
-		defer fd.Close()
-		content, _ := ioutil.ReadAll(fd)
-		fmt.Println(string(content))
-		fd.WriteString(fmt.Sprint(count))
-		time.Sleep(time.Second)
+	// for count, max := 0, 20; count < max; count++ {
+	// 	fd, err := os.OpenFile(filepath, os.O_RDWR, 0)
+	// 	if err != nil {
+	// 		fmt.Printf("fail to open %s", filepath)
+	// 	}
+	// 	defer fd.Close()
+	// 	content, _ := ioutil.ReadAll(fd)
+	// 	fmt.Println(string(content))
+	// 	fd.WriteString(fmt.Sprint(count))
+	// 	time.Sleep(time.Second)
+	// }
+	ffmpegCmdStr := "-i /home/hutianyu/test.mp4 -c copy ./output.mp4 -y"
+
+	pid, err := executeFfmpeg(ffmpegCmdStr)
+	if err == nil {
+		addTasksIntoCgroups("ffmpeg_groups", 150000, 1000000, pid)
+	}
+
+}
+
+func executeFfmpeg(ffmpegCmdStr string) (pid int, err error) {
+	cmdStrs := strings.Split(ffmpegCmdStr, " ")
+	cmd := exec.Command("ffmpeg", cmdStrs...)
+	err = cmd.Start()
+	if err != nil {
+		return 0, err
+	}
+
+	if cmd.Process != nil {
+		return cmd.Process.Pid, nil
+	} else {
+		return 0, nil
+	}
+}
+
+func addTasksIntoCgroups(cgPath string, quota int64, period uint64, pid int) {
+	control, err := cgroups.New(cgroups.V1, cgroups.StaticPath(cgPath), &specs.LinuxResources{
+		CPU: &specs.LinuxCPU{
+			Quota:  &quota,
+			Period: &period,
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer control.Delete()
+	if err = control.AddTask(cgroups.Process{Pid: pid}, "cpu", "ffmpeg_groups"); err != nil {
+		log.Fatal(err)
+		return
+	}
+	if tasks, err := control.Tasks(cgroups.Cpu, false); err == nil {
+		log.Printf("Current tasks: %v", tasks)
 	}
 }
