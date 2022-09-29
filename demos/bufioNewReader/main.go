eb7baa34ccc0ff9208d7e9f3f99b1b67a28e018d
hutianyu <hutianyu@eswincomputing.com>
2022-09-29 11:31:05 +0800

    2022-09-29

--- demos/bufioNewReader/main.go
+++ demos/bufioNewReader/main.go
@@ -14,9 +14,11 @@ func main() {
 	for {
 		line, err := input.ReadString('\n')
 		s := strings.Split(string(line[0:len(line)-1]), ",") // '\n' does not count
+		nums := []int{}
 		for i := 0; i < len(s); i++ {
 			num, _ := strconv.Atoi(s[i])
 			fmt.Printf("num: %v\n", num)
+			//append
 		}
 		if err == io.EOF {
 			fmt.Println("END")
