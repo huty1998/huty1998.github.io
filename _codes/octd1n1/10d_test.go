07617275dba5b2c40a6d08815ed9c8dc7de5715e
hutianyu <hutianyu@eswincomputing.com>
2022-10-10 20:04:19 +0800

    2022-10-10

--- _codes/octd1n1/10d_test.go
+++ _codes/octd1n1/10d_test.go
@@ -8,6 +8,7 @@ import (
 func TestMonoQ(t *testing.T) {
 	fmt.Println(maxSlidingWindow([]int{1, 3, -1, -3, 5, 3, 6, 7}, 3))
 }
+
 func maxSlidingWindow(nums []int, k int) []int {
 	monotonic_q := []int{}
 	var res []int
