cf50ce17616dee2f2966dfb6b3265a8afca8f964
hutianyu <hutianyu@eswincomputing.com>
2022-11-25 15:05:24 +0800

    2022-11-25

--- _codes/novd1n1/nov25_test.go
+++ _codes/novd1n1/nov25_test.go
@@ -17,7 +17,6 @@ func uniquePaths(m, n int) int {
 	for index := range dp {
 		dp[index] = make([]int, n)
 	}
-	dp[0][0] = 1
 
 	for i := 0; i < m; i++ {
 		for j := 0; j < n; j++ {
@@ -32,4 +31,61 @@ func uniquePaths(m, n int) int {
 		}
 	}
 	return -1
+}
+
+func uniquePathsII(obstacleGrid [][]int) int { //?
+	//dp[i][j], (i, j) is the coordinate, dp[i][j] is the total number of paths
+	//At the beginning, we tried to use obstacleGrid as the dp table, but it'll be a mess
+
+	// initialize 2D slice
+	m, n := len(obstacleGrid), len(obstacleGrid[0])
+	dp := make([][]int, m)
+	for index := range dp {
+		dp[index] = make([]int, n)
+	}
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if i == 0 || j == 0 {
+				if obstacleGrid[i][j] != 1 {
+					dp[i][j] = 1
+				}
+			}
+
+		}
+	}
+	// if obstacleGrid[i][j]==1, correspondingly, dp[i][j] remains unchanged at 0
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if obstacleGrid[i][j] != 1 {
+				dp[i][j] = dp[i][j-1] + dp[i-1][j]
+			}
+		}
+	}
+	return dp[m-1][n-1]
+}
+
+func uniquePathsIIV2(obstacleGrid [][]int) int {
+	//obstacleGrid[i][j], (i, j) is the coordinate, obstacleGrid[i][j] is the total number of paths
+	for i := 0; i < len(obstacleGrid); i++ {
+		for j := 0; j < len(obstacleGrid[0]); j++ {
+			if i == 0 || j == 0 {
+				// if i == 0 && obstacleGrid[i][j] == 1 {
+				// 	obstacleGrid[0][j] = 0
+				// }
+				if obstacleGrid[i][j] == 1 { //obstacle
+					obstacleGrid[i][j] = 0
+				} else {
+					obstacleGrid[i][j] = 1
+				}
+			} else {
+				if obstacleGrid[i][j] == 1 {
+					obstacleGrid[i][j] = 0
+					continue
+				}
+				obstacleGrid[i][j] = obstacleGrid[i][j-1] + obstacleGrid[i-1][j]
+			}
+
+		}
+	}
+	return obstacleGrid[len(obstacleGrid)-1][len(obstacleGrid[0])-1]
 }
