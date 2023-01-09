a7e1304bf398e6037f61965399148032f82a31f5
hutianyu <hutianyu@eswincomputing.com>
2023-01-09 20:26:44 +0800

    2023-01-09

--- _codes/Jand1n1/9_test.go
+++ _codes/Jand1n1/9_test.go
@@ -1,13 +1,24 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 //nums = [0,1,2,2,3,0,4,2], val = 2,
 
 func TestSwap(t *testing.T) {
-
+	fmt.Println(removeElement([]int{0, 1, 2, 2, 3, 0, 4, 2}, 2))
 }
 
 func removeElement(nums []int, val int) int {
-
+	slow, fast := 0, 0
+	for ; fast < len(nums); fast++ {
+		if nums[fast] != val {
+			nums[slow], nums[fast] = nums[fast], nums[slow]
+			slow++
+		}
+	}
+	fmt.Println(nums)
+	return slow
 }
