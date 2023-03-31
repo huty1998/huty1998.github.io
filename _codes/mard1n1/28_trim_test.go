46095ff1e2d3906d6b66fab6324957c626341f88
hutianyu <hutianyu@eswincomputing.com>
2023-03-31 17:32:56 +0800

    2023-03-31

---
+++ _codes/mard1n1/28_trim_test.go
@@ -0,0 +1,15 @@
+package mard1n1
+
+func trimBST(root *TreeNode, low int, high int) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val < low {
+		return trimBST(root.Right, low, high)
+	} else if root.Val > high {
+		return trimBST(root.Left, low, high)
+	}
+	root.Left = trimBST(root.Left, low, high)
+	root.Right = trimBST(root.Right, low, high)
+	return root
+}
